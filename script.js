be40ed9fbd2561a80ff61a2e6fd31168168c55a1
Akansha Saxena <66410768+akansha0607@users.noreply.github.com>
2023-02-26 21:46:39 +0530

    "Form initial commit"

--- script.js
+++ script.js
@@ -1,45 +1,172 @@
-let weather = {
-    "apiKey": "7f3974c1698b30704c959c163a0e6d53",
-    fetchWeather: function (city) {
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=" 
-            + city 
-            + "&units=metric&appid=" 
-            + this.apiKey
-        )
-        .then(( response) => response.json())
-        .then((data) => this.displayWeather(data));
-    },
-    displayWeather: function(data) {
-      const { name } = data;
-      const { icon, description } = data.weather[0];
-      const { temp, humidity } = data.main;
-      const { speed } = data.wind;
-     
-      document.querySelector(".city").innerText = "Weather in " + name;
-      document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + ".png";
-      document.querySelector(".description").innerText = description;
-      document.querySelector(".temp").innerText = temp + "°C";
-      document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-      document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
-      document.querySelector(".weather").classList.remove("loading");
-      document.body.style.backgroundImage = "url('https://source.unsplash.com/random/1920x1080/?" + name + "')"
-    },
-    search: function() {
-        this.fetchWeather(document.querySelector(".search-bar").value);
-    },
-};
+//Validate form inputs before submitting data
+function validateForm() {
+  var name = document.getElementById("name").value;
+  var age = document.getElementById("age").value;
+  var address = document.getElementById("address").value;
+  var email = document.getElementById("email").value;
 
-document
-    .querySelector(".search button")
-    .addEventListener("click", function() {
-        weather.search();
-    });
+  if(name == "") {
+    alert("Name is required");
+    return false;
+  }
 
-document
-    .querySelector(".search-bar")
-    .addEventListener("keyup", function(event) {
-       if (event.key == "Enter") {
-        weather.search();
-       }
-    });
+  if(age == "") {
+    alert("Age is required");
+    return false;
+  }
+  else if(age < 1){
+    alert("Age must not be zero or less than zero");
+    return false;
+  }
+
+  if (address == "") {
+    alert("Address is required");
+    return false;
+  }
+
+  if(email == "") {
+    alert("Email is required");
+    return false;
+  }
+  else if(!email.includes("@")) {
+    alert("Invalid email address");
+    return false;
+  }
+
+  return true;
+
+}
+
+//function to show data
+function showData() {
+    var peopleList;
+    if(localStorage.getItem("peopleList") == null) {
+        peopleList = [];
+    }
+    else {
+        peopleList = JSON.parse(localStorage.getItem("peopleList"));
+    }
+
+    var html = "";
+
+    peopleList.forEach(function(element, index){
+        html += "<tr>";
+        html += "<td>" + element.name + "</td>";
+        html += "<td>" + element.age + "</td>";
+        html += "<td>" + element.address + "</td>";
+        html += "<td>" + element.email + "</td>";
+        html += 
+          '<td><button onclick= "deleteData(' +
+          index +
+          ')" class="btn btn-danger">Delete</button><button onclick="updateData(' +
+          index +
+          ')" class="btn btn-warning m-2">Edit</button></td>';
+          html +="</tr>";
+      });
+
+    document.querySelector("#crudTable tbody").innerHTML = 
+    html;   
+}
+
+// Loads All data when document or page loaded
+document.onload = showData();
+
+// function to add data to local storage
+function AddData() {
+    // if form is validate
+    if (validateForm() == true) {
+        var name = document.getElementById("name").value;
+        var age = document.getElementById("age").value;
+        var address = document.getElementById("address").value;
+        var email = document.getElementById("email").value;
+
+        var peopleList;
+        if (localStorage.getItem("peopleList") == null) {
+            peopleList = [];
+
+        } else {
+            peopleList = JSON.parse(localStorage.getItem("peopleList"));
+        }
+
+        peopleList.push({
+            name: name,
+            age: age,
+            address: address,
+            email: email,
+
+        });
+
+        localStorage.setItem("peopleList", JSON.stringify(peopleList));
+        showData();
+        document.getElementById('name').value = "";
+        document.getElementById('age').value = "";
+        document.getElementById('address').value = "";
+        document.getElementById('email').value = "";
+
+    }
+}
+
+//function to delete Data from local storage
+function deleteData(index) {
+    var peopleList;
+    if(localStorage.getItem("peopleList") == null) {
+        peopleList = [];
+    }
+    else {
+        peopleList = JSON.parse(localStorage.getItem("peopleList"));
+    }
+    
+    peopleList.splice(index, 1);
+    localStorage.setItem("peopleList", JSON.stringify
+    (peopleList));
+    showData();
+}
+
+//function to update/edit data in local storage
+function updateData(index) {
+    // Submit button will hide and update button will show for updating of data 
+      // in local storage
+    document.getElementById("Submit").style.display = "none";
+    document.getElementById("Update").style.display = "block";
+
+    var peopleList;
+    if(localStorage.getItem("peopleList") == null) {
+        peopleList = [];
+    }
+    else {
+        peopleList = JSON.parse(localStorage.getItem("peopleList"));
+    }
+
+  document.getElementById("name").value = peopleList[index].name;
+  document.getElementById("age").value = peopleList[index].age;
+  document.getElementById("address").value = peopleList[index].address;  
+  document.getElementById("email").value = peopleList[index].email;
+
+  document.querySelector("#Update").onclick = function() {
+    if (validateForm() == true) {
+    peopleList[index].name = document.getElementById
+    ('name').value;
+    peopleList[index].age = document.getElementById
+    ('age').value;
+    peopleList[index].address = document.getElementById
+    ('address').value;
+    peopleList[index].email = document.getElementById
+    ('email').value;
+
+    localStorage.setItem("peopleList", JSON.stringify(peopleList));
+
+    showData();
+
+    document.getElementById("name").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("address").value = "";
+    document.getElementById("email").value = "";
+
+    // Update button will hide and Submit button will show for updating of data 
+      // in local storage
+      document.getElementById("Submit").style.display = "block";
+      document.getElementById("Update").style.display = "none";
+    }
+  }
+}
+
